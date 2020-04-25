f66ec50e6a194fdcec22343d9a46a9eefc8ece76
Hiroshi Nishihama <ni-hiro102@yk2.so-net.ne.jp>
2020-04-25 15:36:28 +0900

    add hello.c

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * @file hello.c
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2020-04-25
+ * 
+ * @copyright Copyright (c) 2020
+ * 
+ */
+#include <stdio.h>
+
+int main(void)
+{
+    printf("hello world\n");
+    return 0;
+}
